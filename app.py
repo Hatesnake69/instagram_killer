af759e8a19959b32be067e96aad75ca7a538e3a6
Hrabr88 <hrabr88ov@yandex.ru>
2022-04-30 00:53:17 +0300

    add photo gallery

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, flash, request, redirect, url_for, render_template
 
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = 'uploads'
+UPLOAD_FOLDER = 'static/uploads'
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
